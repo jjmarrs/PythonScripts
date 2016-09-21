6cc41f57881391ec0e631be4f090164536a23ad5
JJ <jdmarrs738@gmail.com>
2016-09-21 13:54:09 -0400

    Add Display Grid function Add Play game function Version runs.

---
+++ Games/TicTacToe.py
@@ -0,0 +1,18 @@
+def displayGrid():
+    print("     |        |")
+    print("     |        |")
+    print("     |        |")
+    print("---------------------")
+    print("     |        |")
+    print("     |        |")
+    print("     |        |")
+    print("---------------------")
+    print("     |        |")
+    print("     |        |")
+    print("     |        |")
+
+def playGame():
+    print("\nHello user, welcome to the game of Tic Tac Toe!\n")
+    displayGrid()
+
+playGame()
